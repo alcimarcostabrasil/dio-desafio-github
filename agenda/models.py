9d362c62d93cb82f26d267574e71336f94f46671
Alcimar <alcimar.zeneide@gmail.com>
2022-10-08 04:49:33 -0300

    criação do modelo agenda

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+
+class Agenda(models.Model):
+    foto = StdImageField(upload_to='imagem/contato',
+                         variations={'thumbnail': {'width': 100, 'height': 100}})
+    nome = models.CharField(max_length=255, null=False, blank=False, verbose_name='Nome')
+    telefone = models.CharField(max_length=15, null=False, blank=False, verbose_name='Telefone')
+
+    class Meta:
+        verbose_name = 'Agenda'
+        verbose_name_plural = 'Agendas'
+        db_table = 'agendas'
+
+    def __str__(self):
+        return self.nome
